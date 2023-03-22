b20855a37ce2acef75bc7937727101e53d65d1d4
Arjan Bal <arjansbal@google.com>
2023-03-22 16:24:51 +0530

    Allow multi line commands using shift + enter

--- www/index.js
+++ www/index.js
@@ -3,21 +3,42 @@ import * as wasm from "monkey-interpreter";
 let interpreter = wasm.Wrapper.new()
 
 const prompt = document.getElementById("prompt-input");
-prompt.addEventListener("keyup", function (event) {
-  console.log("here")
+const pressedKeys = {};
+const interestingKeys = ["Enter", "Shift"];
+
+prompt.addEventListener("input", function () {
+  this.style.height = this.scrollHeight + 'px';
+})
+
+prompt.addEventListener("keydown", function (event) {
+  if (interestingKeys.includes(event.key)) {
+    pressedKeys[event.key] = true;
+  }
+  // Ignore if shift + enter.
+  if (pressedKeys["Enter"] && pressedKeys["Shift"]) {
+    return;
+  }
   if (event.key === "Enter") {
+    event.preventDefault();
     const command = prompt.value;
-    prompt.value = '';
     runCommand(command);
+    prompt.value = '';
+  }
+});
+
+prompt.addEventListener("keyup", function (event) {
+  if (interestingKeys.includes(event.key)) {
+    pressedKeys[event.key] = false;
   }
 });
 
 
 const history = document.getElementById("console-history");
+const promptText = document.getElementsByClassName("prompt-text")[0]
 
 function runCommand(command) {
-  history.appendChild(createParaNode(">> " + command));
-  history.appendChild(createParaNode(interpreter.interpret(command)));
+  let output = interpreter.interpret(command);
+  history.appendChild(createHistoryNode(command, output));
   prompt.scrollTop = prompt.scrollHeight;
 }
 
@@ -27,8 +48,32 @@ setInterval(() => {
 
 }, 3000);
 
+function createHistoryNode(command, output) {
+  let commandNode = createDivNode();
+  commandNode.appendChild(promptText.cloneNode(true));
+  commandNode.appendChild(createSpanNode(command));
+  let outputNode = createParaNode(output);
+  let newNode = document.createElement("command");
+  newNode.appendChild(commandNode);
+  newNode.appendChild(outputNode);
+  return newNode;
+}
+
+
+function createDivNode() {
+  return createStyledHistoryElement(null, "div")
+}
+
+function createSpanNode(text) {
+  return createStyledHistoryElement(text, "span")
+}
+
 function createParaNode(text) {
-  let newNode = document.createElement("p");
+  return createStyledHistoryElement(text, "p")
+}
+
+function createStyledHistoryElement(text, elementType) {
+  let newNode = document.createElement(elementType);
   newNode.textContent = text;
   newNode.classList.add("console");
   newNode.classList.add("history-element");
