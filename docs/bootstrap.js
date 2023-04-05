7c29164d280ff53797878764dbfe8734977500c1
GitHub Action <github@actions.com>
2023-04-05 20:13:50 +0000

    Deploying to gh-pages

--- docs/bootstrap.js
+++ docs/bootstrap.js
@@ -162,7 +162,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/monkey_interpreter_wasm_bg.wasm":"a023fdfb9efdb356e4ed"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/monkey_interpreter_wasm_bg.wasm":"06ae44823b84780d36a6"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
